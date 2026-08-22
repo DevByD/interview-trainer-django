33507965787b9219c122dd2db8bb2ddfaee3f5c9
Amol C <amolch001@gmail.com>
2026-08-22 12:05:30 +0530

    added the new files

--- runinterview.sh
+++ runinterview.sh
@@ -13,7 +13,7 @@ if [ -n "$PID" ]; then
 fi
 
 echo "Running migrations..."
-/Users/amolc/2026/interview-trainer-django/venv/bin/python manage.py migrate --noinput
+source /Users/amolc/2026/interview-trainer-django/venv/bin/activate && python manage.py migrate --noinput
 
 echo "Starting Django server on port $PORT..."
-/Users/amolc/2026/interview-trainer-django/venv/bin/python manage.py runserver 0.0.0.0:$PORT
+source /Users/amolc/2026/interview-trainer-django/venv/bin/activate && python manage.py runserver 0.0.0.0:$PORT
